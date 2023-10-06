cd84beb64d02fba3f4f2a6ff12bf92fc9ed83e11
LUCY <wangecikariu.luc@gmail.com>
2023-10-06 20:29:31 +0300

    Lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    #length is 0 or 1 and use if/elif statement
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        #initialize 2 elments and then use while loop
+        return 
+    fibonacci_sequence = [0,1]
+    while len(fibonacci_sequence)<length:
+     next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+    #append fibonacci_sequence
+     fibonacci_sequence.append(next_number)
+
+     # print fibonacci_sequence that has been generated
+    print(fibonacci_sequence)
